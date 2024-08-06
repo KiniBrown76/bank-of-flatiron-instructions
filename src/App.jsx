1d3a2b027f556ca17cf60207cbb1f3b7aff21d1d
KiniBrown76 <esther.kini@student.moringaschool.com>
2024-08-06 07:27:57 +0300

    initial commit without the list in db.json

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import SearchBar from './Components/SearchBar';
+import TransactionForm from './Components/TransactionForm';
+import TransactionTable from './Components/TransactionTable';
+
+function App() {
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch('/transactions')
+      .then(response => response.json())
+      .then(data => setTransactions(data));
+  }, []);
+
+  const addTransaction = (newTransaction) => {
+    setTransactions([...transactions, newTransaction]);
+  };
+
+  const filteredTransactions = transactions.filter(transaction =>
+    transaction.description.UpperCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="container">
+    <header>Bank of Flatiron</header>
+    <aside className="sidebar">
+      <TransactionForm addTransaction={addTransaction} />
+    </aside>
+    <main className="content">
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <TransactionTable transactions={filteredTransactions} />
+    </main>
+  </div>
+  );
+}
+
+export default App;
