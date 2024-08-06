87ef01eb2f2417a70a039ce11e1840bf6bd23962
KiniBrown76 <esther.kini@student.moringaschool.com>
2024-08-06 13:25:52 +0300

    Initial commit with all pagages list on the browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
+// import SearchBar from './components/SearchBar';
+// import TransactionForm from './components/TransactionForm';
+// import TransactionTable from './components/TransactionTable';
 import SearchBar from './Components/SearchBar';
 import TransactionForm from './Components/TransactionForm';
 import TransactionTable from './Components/TransactionTable';
-import TransactionRow from './Components/TransactionRow';
 
 function App() {
   const [transactions, setTransactions] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    fetch('/transactions')
+    fetch('http://localhost:3000/transactions')
       .then(response => response.json())
       .then(data => setTransactions(data));
   }, []);
@@ -19,7 +21,7 @@ function App() {
   };
 
   const filteredTransactions = transactions.filter(transaction =>
-    transaction.description.UpperCase().includes(searchTerm.toLowerCase())
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
@@ -30,7 +32,8 @@ function App() {
     </aside>
     <main className="content">
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      <TransactionTable transactions={filteredTransactions} />
+      {/* <Tran transactions={filteredTransactions} /> */}
+    <TransactionTable transactions={filteredTransactions}/>
     </main>
   </div>
   );
